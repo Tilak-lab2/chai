a42fe968ec3a603f5c082cec5fe941dfac376925
Sritilak Sahu <sritilak007@gmail.com>
2023-11-12 09:07:11 +0530

    util code

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
-import { express } from "express";
+import  express  from "express";
+import cors from "cors"
+import cookieParser from "cookie-parser";
 const app=express()
+app.use(cors(
+    {
+        origin:process.env.CORS_ORIGIN,
+        credentials:true,
 
-
+    }
+))
+app.use(cookieParser())//To use users cookie and perform CRUD operation
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true}))//For getting the url data
+app.use(express.static("public"))
 
 
 export {app}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 // require('dotenv').config()
 import dotenv from "dotenv"
-// import { config } from "dotenv";
+
 import mongoose from "mongoose";
-// import db from "./db/mongoose.js"
+
  import { DB_NAME } from "../constants.js";
 import connectDB from "../db/mongoose.js";
 import express  from "express";
 // const express=require("express")
-// const app=express()
+
+const app=express()
 // ;(async()=>{
 //     try {
 //      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
@@ -21,8 +22,16 @@ import express  from "express";
 //         throw error
 //     }
 // })()
-// config();
+
 dotenv.config({
     path:'./env'
 })
-connectDB()
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 5000,()=>{
+        console.log("Connected");
+    })
+}).catch((err)=>{
+    app.on("error",(err)=>{
+        console.log("Error",err);
+    })
+})
